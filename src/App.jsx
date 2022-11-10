f28b6c24956290540a2fe1bb520af54d7fa21a6b
Federico <fedeaacevedo@gmail.com>
2022-11-10 17:34:10 -0300

    Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "../components/Header";
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import Modal from "../components/Modal";
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
@@ -30,7 +31,9 @@ function App() {
     </div>
     )}
 
-    {modal && <p>Desde modal :)</p>}
+    {modal && <Modal 
+                setModal={setModal}
+              />}
 
       
     </div>
