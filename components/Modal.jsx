8f99f28707b13d963b676a646a6c827e15a2758a
Federico <fedeaacevedo@gmail.com>
2022-11-13 20:03:25 -0300

    Model

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -21,7 +21,37 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
          
          <form className={`formulario ${animarModal ? "animar" : 'cerrar'}`}>
             <legend>Nuevo Gasto</legend>
+          <div className='campo'>
+              <label htmlFor='nombre'>Nombre Gasto</label>
+
+              <input 
+              id='nombre'
+                type='text'
+                placeholder='Añade el nuevo gasto'
+              />
+          </div>
+
+          <div className='campo'>
+              <label htmlFor='cantidad'>Cantidad</label>
+
+              <input 
+              id='cantidad'
+                type='number'
+                placeholder='Añade la cantidad del gasto. Ej: $200'
+              />
+          </div>
+
+          <div className='campo'>
+              <label htmlFor='categoria'>Categoria</label>
+
+              <select id='categoria'>
+                <option value=''>--SELECCIONE--</option>
+              </select>
+          </div>
+         
          </form>
+
+
 
     </div>
   )
